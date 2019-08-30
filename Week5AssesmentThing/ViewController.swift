daac56659fc7e529209ed5ecb385c90c21c4e127
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-30 09:04:08 -0600

    initial commit

---
+++ Week5AssesmentThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week5AssesmentThing
+//
+//  Created by Jackson Tubbs on 8/30/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
